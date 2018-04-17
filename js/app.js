42251e604416b5e5add5a662108ab61de4e88e31
Annija-Cilinska <annija333@gmail.com>
2018-04-17 12:51:45 +0300

    card list updated

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@
  */
 const cardList = [
 	             
-	'fa fa-diamond'
-    'fa fa-paper-plane-o'
-    'fa fa-anchor'
-    'fa fa-bolt'
-    'fa fa-cube'
-    'fa fa-leaf'
-    'fa fa-bicycle'
+	'fa fa-diamond',
+    'fa fa-paper-plane-o',
+    'fa fa-anchor',
+    'fa fa-bolt',
+    'fa fa-cube',
+    'fa fa-leaf',
+    'fa fa-bicycle',
     'fa fa-bomb'
 
 ];
