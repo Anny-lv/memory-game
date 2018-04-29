cdb66258fe2ff0b34396663866e35d513ea29253
Annija-Cilinska <annija333@gmail.com>
2018-04-29 21:26:53 +0300

    timer fixed, modal added

--- js/app.js
+++ js/app.js
@@ -10,16 +10,10 @@ let countMoves = 0;
 const stars = document.querySelector(".stars")
 
 
-
 //Declare empty array for matching cards
 let matchedCards = [];
+let allMatched = [];
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -32,30 +26,20 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
     }
-
     return array;
-}
-
+};
 
 //on reload start a new game
 document.body.onload = newGame();
 
-
-function newGame(){
+	function newGame(){
 
 //Clear matched card collection
 	matchedCards = [];
 
 //Clear moves
 	countMoves = 0;
-	timeStamp();
 	moveCounter();
-
-//Reset stars
-//	    for (let i= 0; i < stars.length; i++){
- //       const star = stars[i].childNodes[0];
- //       star.className.add('fa fa-star');
-// }
 
 
 //shuffle cards and assign new clases to html
@@ -67,20 +51,22 @@ function newGame(){
 		});
 		cards[i].classList.remove("show", "open","disable");
 	}
-
-
 };
-
 
 
 //Add classes to display card
 let displayCard = function (){
     this.classList.add("open");
     this.classList.add("show");
-    
 };
 
-restart.onclick = newGame;
+
+//Restart game when clicking on restart icon
+restart.onclick = reloadFun;
+
+function reloadFun() {
+    location.reload();
+}
 
 //Check if cards match
 function openList(){
@@ -88,8 +74,13 @@ function openList(){
     if (matchedCards.length === 2){
         if (matchedCards[0].firstElementChild.className === matchedCards[1].firstElementChild.className){
             matchCard();
+            allMatched.push(matchedCards[0].firstElementChild.className, matchedCards[1].firstElementChild.className);
             matchedCards = [];
             moveCounter();
+            if (allMatched.length==16) {
+            	gameOver ();
+
+            }
         } else {    
             unmatchCard();
             moveCounter();
@@ -123,7 +114,7 @@ function moveCounter (){
 
 	if(countMoves >=  12 && countMoves < 17)	{
 		stars.children[2].innerHTML = "";
-	}
+			}
 	else if (countMoves >=  17 && countMoves < 24)	{
 		stars.children[1].innerHTML = "";
 	}
@@ -133,45 +124,65 @@ function moveCounter (){
 }
 
 
+//Start the timer when new game is started
+let secondsElapsed = 0;
 
-//Count up timer, source: https://stackoverflow.com/questions/5517597/plain-count-up-timer-in-javascript
+let x = setInterval(function() {
+  if (newGame) {
+    secondsElapsed++;
+ };
 
-function timeStamp() {
-// Create a date object with the current time
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
-setInterval(setTime, 1000);
+document.getElementsByClassName("timer")[0].innerHTML =
+  `<i class='fa fa-clock-o'></i>
+  ${(Math.floor(secondsElapsed / 60) < 10) ?
+    ('0' + Math.floor(secondsElapsed / 60)) :
+    Math.floor(secondsElapsed / 60)}:${
+    (secondsElapsed % 60 < 10) ?
+    ('0' + secondsElapsed % 60) : secondsElapsed % 60}`;
+  }, 1000
+ 
+);
 
-function setTime() {
-  ++totalSeconds;
-  secondsLabel.innerHTML = pad(totalSeconds % 60);
-  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+function gameOver() {
+    modal.style.display = "block";
+    secondsElapsed =0;
+};
+
+//Modal
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+   document.getElementsByClassName("timer")[0].innerHTML = secondsElapsed;
 }
 
-function pad(val) {
-  var valString = val + "";
-  if (valString.length < 2) {
-    return "0" + valString;
-  } else {
-    return valString;
- 	 	}
-	}
- }
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
+////
 for (let i = 0; i < cards.length; i++){
     card = cards[i];
     card.addEventListener('click', displayCard);
     card.addEventListener('click', openList);
     };
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+
